04600acf1b282124a7cac40251fc2d6d3f4e8cd6
Mert Çetin <102957602+mertcetiin@users.noreply.github.com>
2023-08-23 15:46:47 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,7 @@ function App() {
     <div className='App'>
       <Navbar />
       <Routes>
-        <Route path='/homePage' element={<HomePage />} />
+        <Route path='' element={<HomePage />} />
         <Route path='/menu' element={<Menu />} />
         <Route path='/aboutUs' element={<AboutUs />} />
         <Route path='/communication' element={<Communication />} />
