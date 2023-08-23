b587649611f07f2176b3cde8b855bf843d658a18
Mert Çetin <mrtctn506@gmail.com>
2023-08-23 15:10:41 +0300

    Menu.css made

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,111 +1,25 @@
 import React from 'react'
-import burger from '../assets/img/burger.jpg'
+import { Data } from '../helpers/Data.jsx'
+import '../styles/Menu.css'
 
 
 function Menu() {
 
-    const burgerMenu = [
-        {
-            id: 1,
-            img: '',
-            name: 'Efso Burger',
-            info: '150 gr kasap Burger,Patates,Turşu',
-            price: '250 TL'
-        },
-        {
-            id: 2,
-            img: '',
-            name: 'Efso Burger',
-            info: '150 gr kasap Burger,Patates,Turşu',
-            price: '250 TL'
-        },
-        {
-            id: 3,
-            img: '',
-            name: 'Efso Burger',
-            info: '150 gr kasap Burger,Patates,Turşu',
-            price: '250 TL'
-        },
-        {
-            id: 4,
-            img: '',
-            name: 'Efso Burger',
-            info: '150 gr kasap Burger,Patates,Turşu',
-            price: '250 TL'
-        },
-        {
-            id: 5,
-            img: '',
-            name: 'Efso Burger',
-            info: '150 gr kasap Burger,Patates,Turşu',
-            price: '250 TL'
-        },
-        {
-            id: 6,
-            img: '',
-            name: 'Efso Burger',
-            info: '150 gr kasap Burger,Patates,Turşu',
-            price: '250 TL'
-        },
-        {
-            id: 7,
-            img: '',
-            name: 'Efso Burger',
-            info: '150 gr kasap Burger,Patates,Turşu',
-            price: '250 TL'
-        },
-        {
-            id: 8,
-            img: '',
-            name: 'Efso Burger',
-            info: '150 gr kasap Burger,Patates,Turşu',
-            price: '250 TL'
-        },
-        {
-            id: 9,
-            img: '',
-            name: 'Efso Burger',
-            info: '150 gr kasap Burger,Patates,Turşu',
-            price: '250 TL'
-        },
-        {
-            id: 10,
-            img: '',
-            name: 'Efso Burger',
-            info: '150 gr kasap Burger,Patates,Turşu',
-            price: '250 TL'
-        },
-        {
-            id: 11,
-            img: '',
-            name: 'Efso Burger',
-            info: '150 gr kasap Burger,Patates,Turşu',
-            price: '250 TL'
-        },
-        {
-            id: 12,
-            img: '',
-            name: 'Efso Burger',
-            info: '150 gr kasap Burger,Patates,Turşu',
-            price: '250 TL'
-        },
-
-    ]
-
     return (
-        <div className='menuDiv'>
-            <h1 className='menu-h1'>Burgerlerimiz</h1>
-            {burgerMenu.map((item) => (
-                <div className='menuCard' key={item.id}>
-                    <img src={burger} />
-                    <h1>{item.name}</h1>
-                    <h4>{item.info}</h4>
-                    <h2>{item.price}</h2>
-                </div>
-            ))}
+        <div className='menu'>
+            <h1 className='menuTitle'>Burgerlerimiz</h1>
+            <div className='menuList'>
+                {Data.map((item) => (
+                    <div key={item.id}>
+                        <img src={item.img} />
+                        <h1>{item.name}</h1>
+                        <h4>{item.info}</h4>
+                        <h2>{item.price}</h2>
+                    </div>
+                ))}
+            </div>
         </div>
-
     )
 }
 
-export default Menu
+export default Menu;
---
+++ src/helpers/Data.jsx
@@ -0,0 +1,89 @@
+import React from 'react'
+import BurgerImage from '../assets/img/burger.jpg'
+
+export const Data = [
+    {
+        id: 1,
+        img: BurgerImage,
+        name: 'Efso Burger',
+        info: '150 gr kasap Burger,Patates,Turşu',
+        price: '250 TL'
+    },
+    {
+        id: 2,
+        img: BurgerImage,
+        name: 'Efso Burger',
+        info: '150 gr kasap Burger,Patates,Turşu',
+        price: '250 TL'
+    },
+    {
+        id: 3,
+        img: BurgerImage,
+        name: 'Efso Burger',
+        info: '150 gr kasap Burger,Patates,Turşu',
+        price: '250 TL'
+    },
+    {
+        id: 4,
+        img: BurgerImage,
+        name: 'Efso Burger',
+        info: '150 gr kasap Burger,Patates,Turşu',
+        price: '250 TL'
+    },
+    {
+        id: 5,
+        img: BurgerImage,
+        name: 'Efso Burger',
+        info: '150 gr kasap Burger,Patates,Turşu',
+        price: '250 TL'
+    },
+    {
+        id: 6,
+        img: BurgerImage,
+        name: 'Efso Burger',
+        info: '150 gr kasap Burger,Patates,Turşu',
+        price: '250 TL'
+    },
+    {
+        id: 7,
+        img: BurgerImage,
+        name: 'Efso Burger',
+        info: '150 gr kasap Burger,Patates,Turşu',
+        price: '250 TL'
+    },
+    {
+        id: 8,
+        img: BurgerImage,
+        name: 'Efso Burger',
+        info: '150 gr kasap Burger,Patates,Turşu',
+        price: '250 TL'
+    },
+    {
+        id: 9,
+        img: BurgerImage,
+        name: 'Efso Burger',
+        info: '150 gr kasap Burger,Patates,Turşu',
+        price: '250 TL'
+    },
+    {
+        id: 10,
+        img: BurgerImage,
+        name: 'Efso Burger',
+        info: '150 gr kasap Burger,Patates,Turşu',
+        price: '250 TL'
+    },
+    {
+        id: 11,
+        img: BurgerImage,
+        name: 'Efso Burger',
+        info: '150 gr kasap Burger,Patates,Turşu',
+        price: '250 TL'
+    },
+    {
+        id: 12,
+        img: BurgerImage,
+        name: 'Efso Burger',
+        info: '150 gr kasap Burger,Patates,Turşu',
+        price: '250 TL'
+    },
+]
